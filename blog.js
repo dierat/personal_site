301819a1c6969bce213b85124eca38559dc7e294
dierat <diedra.rater@gmail.com>
2015-08-24 21:10:38 -0700

    wraps img in figure so we can center it

--- blog.js
+++ blog.js
@@ -15,7 +15,16 @@ if (Meteor.isClient){
   });
   Template.post.helpers({
     paragraphs: function(){
-      return Posts.findOne({_id: this._id}).content;
+      return Posts.findOne({_id: this._id}).content.map(function(paragraph){
+        if (typeof paragraph === 'string'){
+          return paragraph;
+        } else if (typeof paragraph === 'object'){
+          if (paragraph.src) {
+            return '<figure><img src=' + paragraph.src + '></figure>'
+          }
+        }
+      });;
+
     }
   });
   // Template.tags.helpers({
@@ -41,7 +50,7 @@ if (Meteor.isServer){
           tags: ["JavaScript", "programming"]
         }, {
           title: "Second Post",
-          content: ["<img src='http://www.clker.com/cliparts/9/2/8/c/1195421944619161676typewriter_john_olsen_01.svg.hi.png'>", "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"],
+          content: [{src: 'http://www.clker.com/cliparts/9/2/8/c/1195421944619161676typewriter_john_olsen_01.svg.hi.png'}, "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"],
           tags: ["blue", "moonlight", "martian"]
         }, {
           title: "Third Post",
